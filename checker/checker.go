73a0acdcde689e0decaf522bde6f2c92021dd774
Simo Aleksandrov <simo3003@me.com>
2019-07-04 02:41:11 +0300

    finish worker?

--- checker/checker.go
+++ checker/checker.go
@@ -2,62 +2,72 @@ package checker
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 
 	"github.com/fr3fou/go-steamapi"
 )
 
 type idChecker struct {
-	id  string
-	key string
+	id      string
+	key     string
+	isTaken bool
 }
 
 // CheckIds takes in an io.Reader and calls the Steam API against each word in
 // the reader with workerAmont of workers to check whether the given ID exists
 // TODO think of what should this method return so that it can be used anywhere
 func CheckIds(words io.Reader, key string, workerAmount int) {
-	fileScanner := bufio.NewScanner(words)
+	wordsScanner := bufio.NewScanner(words)
 
 	// TODO what should the buffer size be?
 	// I think it has to be the total amount of words (but how do I get them from the reader?)
 	jobs := make(chan idChecker)
-	results := make(chan *idChecker)
+	results := make(chan idChecker)
 
 	// Start up workerAmount of workers
 	for w := 1; w < workerAmount; w++ {
 		go worker(jobs, results)
 	}
 
+	limit := 1
+
 	// Fill in the jobs queue (channel)
-	for fileScanner.Scan() {
-		id := fileScanner.Text()
+	for ; wordsScanner.Scan(); limit++ {
+		id := wordsScanner.Text()
 		jobs <- idChecker{
-			id,
-			key,
+			id:      id,
+			key:     key,
+			isTaken: false,
 		}
 	}
 
 	close(jobs)
+
+	for i := 0; i < limit; i++ {
+		result := <-results
+		if !result.isTaken {
+			// Probs change to something else other than fmt.Println?
+			fmt.Printf("(%d out of %d) - %s is not taken on Steam!", i, limit, result.id)
+		} else {
+			// Probs change to something else other than fmt.Println?
+			fmt.Printf("(%d out of %d) - %s is taken on Steam.", i, limit, result.id)
+		}
+	}
+
+	close(results)
 }
 
-func worker(jobs <-chan idChecker, results chan<- *idChecker) {
+func worker(jobs <-chan idChecker, results chan<- idChecker) {
 	// for every job in the queue (channel), call checkID
 	for j := range jobs {
 		results <- checkID(j)
 	}
 }
 
-func checkID(ic idChecker) *idChecker {
-	resp, err := steamapi.ResolveVanityURL(ic.id, ic.key)
-
-	if err != nil {
-		return nil
-	}
-
-	// According to the documentation, 42 is the code returned when an ID doesn't exist
-	if resp.Response.Success == 42 {
-		return &ic
-	}
-
-	return nil
+func checkID(ic idChecker) idChecker {
+	// TODO: error handling
+	resp, _ := steamapi.ResolveVanityURL(ic.id, ic.key)
+	ic.isTaken = resp.Response.Success == 1
+	return ic
 }
