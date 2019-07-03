e8031ff3e28405c7b0360392f801037507a0cfdb
Simo Aleksandrov <simo3003@me.com>
2019-07-04 01:24:03 +0300

    Add check for steam api key

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/fr3fou/go-steamapi"
 )
@@ -16,17 +19,52 @@ func main() {
 	scanner.Scan()
 	path := scanner.Text()
 
-	fmt.Print("Enter enter your Steam API key (you can get yours at https://steamcommunity.com/dev/apikey): ")
-
-	scanner.Scan()
-	key := scanner.Text()
-
 	file, err := os.Open(path)
 
 	if err != nil {
 		log.Fatal("The path you entered isn't a valid file!", err)
 	}
 
+	key := ""
+
+	contents, err := ioutil.ReadFile(".key")
+
+	// If we have read the file successfully, this means that there must be a key inside
+	if err == nil {
+		fmt.Printf("An existing Steam API key has been found (%s...), would you like to use it? (Y/n): ", string(contents)[:6])
+		scanner.Scan()
+		useExisting := scanner.Text()
+
+		if checkForAgreement(useExisting) {
+			key = string(contents)
+		}
+	}
+
+	// If the key still empty, ask for it
+	if key == "" {
+		fmt.Print("Enter enter your Steam API key (you can get yours at https://steamcommunity.com/dev/apikey): ")
+
+		scanner.Scan()
+		key = scanner.Text()
+
+		fmt.Print("Would you like to remember the key for future use? (Y/n): ")
+
+		scanner.Scan()
+		remember := strings.Trim(scanner.Text(), " ")
+
+		if checkForAgreement(remember) {
+			ioutil.WriteFile(".key", []byte(key), 0644)
+		}
+	}
+
+	checkIds(file, key)
+}
+
+func checkForAgreement(s string) bool {
+	return s == "y" || s == "Y" || s == "Yes" || s == "yes" || s == ""
+}
+
+func checkIds(file *os.File, key string) {
 	fileScanner := bufio.NewScanner(file)
 
 	for fileScanner.Scan() {
@@ -39,5 +77,4 @@ func main() {
 
 		fmt.Println(resp.Response.Success)
 	}
-
 }
