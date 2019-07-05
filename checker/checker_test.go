326c46d0cc0095e8b2dac755031053b0e654f5dd
Simo Aleksandrov <simo3003@me.com>
2019-07-05 23:10:06 +0300

    more tests

--- checker/checker_test.go
+++ checker/checker_test.go
@@ -5,17 +5,23 @@ import (
 	"testing"
 )
 
-func TestCheckIDWithAPI(t *testing.T) {
-	res := make(chan SteamID, 1)
+func TestCheckID(t *testing.T) {
+	res := make(chan SteamID, 2)
 	var wg sync.WaitGroup
 
 	wg.Add(1)
 	checkID("fr3fou", &wg, res)
-	wg.Wait()
 
 	taken := <-res
 	if !taken.IsTaken {
 		t.Error("CheckIDs(\"fr3fou\", &wg, res) returned IsTaken = false; expected true")
 	}
 
+	wg.Add(1)
+	checkID("asdfasdfasdfasdfasdf0a9sd8f0asd8f90as8d09fa8sd09fa8s0df", &wg, res)
+
+	taken = <-res
+	if taken.IsTaken {
+		t.Error("CheckIDs(\"asdfasdfasdfasdfasdf0a9sd8f0asd8f90as8d09fa8sd09fa8s0df\", &wg, res) returned IsTaken = true; expected false")
+	}
 }
