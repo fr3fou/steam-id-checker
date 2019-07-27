f5a5ffc3d661bf10744f172b70235f5f2578b916
Simo Aleksandrov <simo3003@me.com>
2019-07-27 21:00:05 +0300

    update tests

--- checker/checker_test.go
+++ checker/checker_test.go
@@ -8,7 +8,7 @@ func TestCheckID(t *testing.T) {
 	taken, err := checkID("fr3fou")
 
 	if err != nil {
-		t.Errorf("CheckIDs(\"fr3fou\") returned err = %s; expected nil", err.Error())
+		t.Errorf("CheckIDs(\"fr3fou\") returned err = %v; expected nil", err.Error())
 	} else if !taken.IsTaken {
 		t.Error("CheckIDs(\"fr3fou\") returned IsTaken = false; expected true")
 	}
@@ -16,7 +16,7 @@ func TestCheckID(t *testing.T) {
 	free, err := checkID("asdfasdfasdfasdfasdf0a9sd8f0asd8f90as8d09fa8sd09fa8s0df")
 
 	if err != nil {
-		t.Errorf("CheckIDs(\"asdfasdfasdfasdfasdf0a9sd8f0asd8f90as8d09fa8sd09fa8s0df\") returned err = %s; expected nil", err.Error())
+		t.Errorf("CheckIDs(\"asdfasdfasdfasdfasdf0a9sd8f0asd8f90as8d09fa8sd09fa8s0df\") returned err = %v; expected nil", err.Error())
 	} else if free.IsTaken {
 		t.Error("CheckIDs(\"asdfasdfasdfasdfasdf0a9sd8f0asd8f90as8d09fa8sd09fa8s0df\") returned IsTaken = true; expected false")
 	}
